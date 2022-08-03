5ae083b0539558be8805210f8128e1685d18366c
21034612 <21034612@10.174.123.151>
2022-08-03 15:49:12 +0800

    Commit By Joey

---
+++ C206_CaseStudy/src/enquiry.java
@@ -0,0 +1,61 @@
+
+public class enquiry{
+	private String EnquiryId;
+	private String Title;
+	private String Date;
+	private String FollowupResponses;
+	private boolean Urgency;
+
+	public enquiry(String EnquiryId, String Title, String FollowupResponses,String Date,boolean Urgency) {
+		this.EnquiryId = EnquiryId;
+		this.Title = Title;
+		this.FollowupResponses = FollowupResponses;
+		this.Date = Date;
+		this.Urgency = Urgency;
+		
+	}
+	public String getEnquiryId() {
+		return EnquiryId;
+	}
+	public void setEnquiryId(String enquiryId) {
+		EnquiryId = enquiryId;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getFollowupResponses() {
+		return FollowupResponses;
+	}
+	public void setFollowupResponses(String followupResponses) {
+		FollowupResponses = followupResponses;
+	}
+	public boolean isUrgency() {
+		return Urgency;
+	}
+	public void setUrgency(boolean urgency) {
+		Urgency = urgency;
+	}
+	public static String showUrgency(boolean isUrgency) {
+		String Urgency;
+
+		if (isUrgency == true) {
+			Urgency = "Yes";
+		} else {
+			Urgency = "No";
+		}
+		return Urgency;
+	}
+	public static void setHeader(String string) {
+		
+	}
+}
+
